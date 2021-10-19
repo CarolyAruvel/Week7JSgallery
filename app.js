e708a0f10396601d3600dc5717bc97a59739d70e
Opilane TTHK <opilane@tthk.ee>
2021-10-19 13:40:40 +0300

    add app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const currentImage = document.querySelector("#image");
+
+const images = ["images/pilt1.jpeg", "images/pilt2.webp", "images/pilt3.jpeg"]
+
+function changeImage() {
+    let randomNumber = Math.floor(Math.random() * images.length);
+    console.log(randomNumber);
+    currentImage.src = images[randomNumber];
+}
+
+
